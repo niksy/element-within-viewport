90400b2e62532fc5fff889a9e842d3718fee241d
Ivan Nikolić <niksy5@gmail.com>
2014-05-17 12:27:04 +0200

    Release 0.4.0

--- dist/kist-inview.js
+++ dist/kist-inview.js
@@ -1,32 +1,19 @@
-/* kist-inview 0.3.0 - Check if elements are in viewport. | Author: Ivan Nikolić, 2014 | License: MIT */
+/* kist-inview 0.4.0 - Check if elements are in viewport. | Author: Ivan Nikolić, 2014 | License: MIT */
 ;(function ( $, window, document, undefined ) {
 
-	var pluginNamespace = 'kist';
-	var pluginName      = 'inView';
+	var dom = {
+		window: $(window)
+	};
+	var windowCoords = {
+		top: 0,
+		bottom: 0
+	};
 
 	var InView = function () {
-
-		var setupDomRefs = function () {
-			this.dom        = {};
-			this.dom.window = $(window);
-		};
-
-		setupDomRefs.call(this);
 		this.calculateViewport();
-
 	};
 
 	$.extend(InView.prototype, {
-
-		/**
-		 * Default options
-		 *
-		 * @type {Object}
-		 */
-		defaults: {
-			windowTop: 0,
-			windowBottom: 0
-		},
 
 		/**
 		 * Calculate viewport
@@ -35,8 +22,8 @@
 		 */
 		calculateViewport: function () {
 
-			this.defaults.windowTop    = this.dom.window.scrollTop();
-			this.defaults.windowBottom = this.defaults.windowTop + this.dom.window.height();
+			windowCoords.top    = dom.window.scrollTop();
+			windowCoords.bottom = windowCoords.top + dom.window.height();
 
 		},
 
@@ -45,21 +32,16 @@
 		 *
 		 * @param  {jQuery}  el
 		 * @param  {Number}  threshold
-		 * @param  {Boolean}  viewportCalculated
 		 *
 		 * @return {Boolean}
 		 */
-		isVisible: function ( el, threshold, viewportCalculated ) {
+		isVisible: function ( el, threshold ) {
 
 			var elTop    = el.offset().top;
 			var elBottom = elTop + el.height();
 			threshold    = threshold || 0;
 
-			if ( !viewportCalculated ) {
-				this.calculateViewport();
-			}
-
-			return elBottom >= this.defaults.windowTop - threshold && elTop <= this.defaults.windowBottom + threshold;
+			return elBottom >= windowCoords.top - threshold && elTop <= windowCoords.bottom + threshold;
 
 		},
 
@@ -77,7 +59,7 @@
 
 			return el.filter($.proxy(function ( index, element ) {
 
-				return this.isVisible( $(element), threshold, true );
+				return this.isVisible( $(element), threshold );
 
 			}, this));
 
@@ -87,25 +69,8 @@
 
 	var o = new InView();
 
-	$.fn[pluginNamespace] = $.fn[pluginNamespace] || {};
-
-	$.fn[pluginNamespace][pluginName] = function ( method, threshold ) {
-		/* jshint -W086 */
-
-		switch ( method ) {
-			case 'state': {
-				return Boolean( o.getElements( this, threshold ).length );
-			}
-			case 'elements': {
-				return o.getElements( this, threshold );
-			}
-			default: {
-				throw new Error( pluginName + ': Method is either undefined or doesn’t exist.' );
-			}
-		}
-
+	$.fn.inView = function ( threshold ) {
+		return o.getElements( this, threshold );
 	};
-
-	$.fn[pluginName] = $.fn[pluginName] || $.fn[pluginNamespace][pluginName];
 
 })( jQuery, window, document );
